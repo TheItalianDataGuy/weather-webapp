cc2ae80402b22cf45e01ea46fbb2c32f08a38f95
Andrew <andreamarella0@gmail.com>
2024-06-03 06:20:18 +0100

    Added graph and function to get data (fake)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import plotly.express as px
 
 # User interface
 st.title("Weather Forecast for the Next Days")
@@ -8,5 +9,21 @@ day = st.slider("Forecast day: ", min_value=1, max_value=5,
 option = st.selectbox("Select data to view",
                       ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {day} days in {place}")
+
+
+# Create a graph with plotly
+# Define a function to get the data
+def get_data (day_local):
+    dates = ["2022-25-10", "2022-26-10", "2022-27-10"]
+    temperatures = ["10", "11", "17"]
+    temperatures = [day * i for i in temperatures]
+    return dates, temperatures
+
+
+# Split the return of the function so d=date and t=temperature
+d, t = get_data(day)
+
+figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C*)"})
+st.plotly_chart(figure)
 
 
